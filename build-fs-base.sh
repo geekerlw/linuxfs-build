1dad97879f52a837c5767b72abe13a6e3277b3af
geekerlw <geekerlw@gmail.com>
2016-12-23 20:02:08 +0800

    change the way to overlay presets and upgrade debs.
    1. upgrade libmali to r9po-r0p0-1.5-2
    2. upgrade xserver to 1.18.4
    3. remove libva support
    4. add libdrm support
    5. add gstreamer-rk and mpp to sopport video decode
    6. change the way to overlay presets

--- build-fs-base.sh
+++ build-fs-base.sh
@@ -2,20 +2,26 @@
 
 # Base root filesystem build script
 # set TARGET_ROOTFS_DIR by yourself, default dir in your build path named ubuntu
-export TARGET_ROOTFS_DIR="ubuntu"
-export ARCH="armhf"
-export HOSTNAME="rk3288"
-export USERNAME="cqutprint"
+TARGET_ROOTFS_DIR="ubuntu"
+ARCH="armhf"
+if [ ! $BOARDNAME ]; then
+	BOARDNAME="rk3288"
+fi
+if [ ! $USERNAME ]; then
+	USERNAME="cqutprint"
+fi
 
 # set ubuntu version
-export ROOTFS_VERSION="16.04.1"
+if [ ! $ROOTFS_VERSION ]; then
+	ROOTFS_VERSION="16.04.1"
+fi
 
 # create target dir and clean
 if [ ! -e $TARGET_ROOTFS_DIR ]; then
 	echo "make target dir"
-	mkdir ubuntu
+	mkdir $TARGET_ROOTFS_DIR
 fi
-sudo rm -rvf ubuntu/*
+sudo rm -rvf $TARGET_ROOTFS_DIR/*
 
 # get ubuntu base filesystem
 if [ ! -e ubuntu-base-$ROOTFS_VERSION-base-armhf.tar.gz ]; then
@@ -28,20 +34,20 @@ echo "extract image"
 sudo tar -xvpf ubuntu-base-$ROOTFS_VERSION-base-armhf.tar.gz -C $TARGET_ROOTFS_DIR
 
 # replace sources.list and dns server
-sudo cp -av ./preset/network/sources.list $TARGET_ROOTFS_DIR/etc/apt/sources.list
+sudo cp -av ./preset/overlay/etc/apt/sources.list $TARGET_ROOTFS_DIR/etc/apt/sources.list
 sudo cp -bv /etc/resolv.conf $TARGET_ROOTFS_DIR/etc/resolv.conf
 
 # prepare the environment to chroot
 sudo cp -av ./prebuild/ $TARGET_ROOTFS_DIR/
 sudo cp -av ./preset/ $TARGET_ROOTFS_DIR/
 sudo cp -av /usr/bin/qemu-arm-static $TARGET_ROOTFS_DIR/usr/bin/ 
-sudo mount -o bind /dev $TARGET_ROOTFS_DIR/dev
+# sudo mount -o bind /dev $TARGET_ROOTFS_DIR/dev
 
 # chroot to install some packages
 cat << EOF | sudo chroot $TARGET_ROOTFS_DIR
 apt update && apt upgrade -y
 
-#........timezone locale and keyboard..........
+# timezone locale and keyboard
 echo "UTC=yes" >> /etc/default/rcS
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
@@ -50,66 +56,67 @@ echo "LANG="zh_CN.UTF-8"" > /etc/default/locale
 echo "LANGUAGE="zh_CN:en_GB:en"" >> /etc/default/locale
 echo "XKBLAYOUT="us"" >> /etc/default/keyboard
 
-#........install some base tools..........
+# install some base tools
 apt install -y xfce4 net-tools iputils-ping wicd vim
 
-#.........transitional chinese support....
-apt install -y fonts-wqy-microhei
-
-#........cups and hp printer driver.......
-apt install -y cups hplip
-
-#........Split screen tool................
-apt install -y devilspie
-
-#................xserver...................
-dpkg -X ./prebuild/xserver/xserver-common_*_all.deb /
-dpkg -X ./prebuild/xserver/xserver-xorg-core_*_armhf.deb /
-
-#................maligpu..................
-dpkg -X ./prebuild/libmali/libmali-rk-midgard0_*_armhf.deb /
-dpkg -X ./prebuild/libmali/libmali-rk-dev_*_armhf.deb /
-rm -rvf /usr/lib/arm-linux-gnueabihf/mesa-egl/*
-echo "KERNEL=="mali0", MODE="0666", GROUP="video"" > /etc/udev/rules.d/50-mali.rules
-
-#..........gstreamer and vaapi............
+# gstreamer base tools
 apt install -y gstreamer1.0-alsa gstreamer1.0-nice gstreamer1.0-plugins-bad \
 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-tools \
-gstreamer1.0-x gstreamer1.0-plugins-bad-faad gstreamer1.0-vaapi
-dpkg -X ./prebuild/vaapi/libva-rockchip*_armhf.deb / 
+gstreamer1.0-x gstreamer1.0-plugins-bad-faad
 
-#........enable dhcp network.............
+# transitional chinese support
+apt install -y fonts-wqy-microhei
+
+# cups and hp printer driver
+apt install -y cups hplip
+
+# Split screen tool
+apt install -y devilspie
+
+# xserver
+dpkg -x ./prebuild/xserver/xserver-common*_all.deb /
+dpkg -x ./prebuild/xserver/xserver-xorg-core*_armhf.deb /
+
+# maligpu
+dpkg -i ./prebuild/libmali/libmali-rk-midgard*_armhf.deb 
+dpkg -i ./prebuild/libmali/libmali-rk-dev*_armhf.deb 
+rm -rvf /usr/lib/arm-linux-gnueabihf/mesa-egl/*
+
+# libdrm
+dpkg -i ./prebuild/libdrm/libdrm-rockchip*_armhf.deb
+
+# mpp
+dpkg -i ./prebuild/mpp/librockchip-mpp*_armhf.deb
+dpkg -i ./prebuild/mpp/librockchip-vpu*_armhf.deb
+
+
+# gstreamer-rockchip
+dpkg -i ./prebuild/gstreamer-rockchip/gstreamer1.0-rockchip*_armhf.deb
+
+# enable dhcp network
 echo auto eth0 > /etc/network/interfaces.d/eth0
 echo iface eth0 inet dhcp >> /etc/network/interfaces.d/eth0
 
-#.......enable autologin from tty1.........
-cp -av ./preset/getty/getty@tty1.service \
-/etc/systemd/system/getty.target.wants/getty@tty1.service
+# hostname
+echo $BOARDNAME > /etc/hostname 
 
-# rename hostname
-echo rk3288 > /etc/hostname 
+# add user name and passwd default cqutprint
+useradd -m -s /bin/bash $USERNAME
+echo "$USERNAME:$USERNAME" | chpasswd
+echo "root:root" | chpasswd
 
-# add user named cqutprint
-useradd -m -s /bin/bash cqutprint
+# overlay the presets
+cp -av ./preset/overlay/* /
+cp -av ./preset/user/. /home/$USERNAME/
 
-#.......enable autologin xfce from tty1.........
-cp -av ./preset/xfce4/bash_profile /home/cqutprint/.bash_profile
-cp -av ./preset/xfce4/xinitrc /home/cqutprint/.xinitrc
-cp -av ./preset/xfce4/xserverrc /home/cqutprint/.xserverrc
-
-#........remove package cache..............
+# remove package cache
 rm -rvf /var/cache/apt/archives/*
 rm -rvf /prebuild
 rm -rvf /preset
 EOF
 
 
-# replace some preset here
-sudo cp -av ./preset/rules/X11/20-modesetting.conf \
-$TARGET_ROOTFS_DIR/usr/share/X11/xorg.conf.d/
-sudo cp -av ./preset/rules/udev/50-mali.rules $TARGET_ROOTFS_DIR/etc/udev/rules.d/
-
 # end chroot
-sudo umount $TARGET_ROOTFS_DIR/dev
+# sudo umount $TARGET_ROOTFS_DIR/dev
 
 echo "build successful,enjoy!!!"
